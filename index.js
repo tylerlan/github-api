740d4d139f0e609c664289ad6720298c7e215784
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-08-19 19:20:34 -0700

    Allow cross domain methods

--- index.js
+++ index.js
@@ -4,22 +4,40 @@ const graphqlHTTP = require('express-graphql');
 const root = require('./src/resolvers/RootResolver');
 const schema = require('./src/schema/schema');
 
-
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
+
+const allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept',
+  );
+  if (req.method == 'OPTIONS') {
+    res.send(200);
+  } else {
+    next();
+  }
+};
 
 app.use(bodyParser.json());
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  rootValue: root,
-  graphiql: true,
-}));
+app.use(allowCrossDomain);
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true,
+  }),
+);
 
 app.use((req, res) => {
   res.sendStatus(404);
 });
-
 
 if (!module.parent) {
   app.listen(PORT, () => {
